0e2a91dbd0b684ef7774853c2d7d9a126ab7b566
DarksergZ <32735373+DarksergZ@users.noreply.github.com>
2018-03-28 15:15:54 +0300

    Add files via upload

---
+++ script1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import os 
+#import time
+
+print("Starting script Top hashtags !")
+os.system("python model_clasters_top_hashtags_for5.py twitter_1.json twitter_2.json")
